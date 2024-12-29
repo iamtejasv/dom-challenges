6577bbc573764ee765a6461997f652e83d85f277
iamtejasv <tejasv0804@gmail.com>
2024-12-29 14:16:30 +0530

    add challenge 1

---
+++ script.js
@@ -0,0 +1,7 @@
+// challenge 1
+
+document.getElementById("changeTextButton").addEventListener('click', function ()  {
+    let paragraph = document.getElementById("myParagraph");
+    paragraph.textContent = "the paragraph is changed";
+    
+})
