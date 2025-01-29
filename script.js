1a1e439c12de2d6482ac3d910873b4e0a11cff36
iamtejasv <tejasv0804@gmail.com>
2025-01-29 14:08:06 +0530

    add challenge 8

--- script.js
+++ script.js
@@ -54,3 +54,13 @@ document.getElementById("teaList").addEventListener("click", function(event) {
   }
   
 })
+
+// challenge 8
+
+document.getElementById("feedbackForm").addEventListener('submit', function(event) {
+  event.preventDefault();
+  let feedback = document.getElementById("feedbackInput").value;
+  console.log(feedback);
+  document.getElementById("feedbackDisplay").textContent =
+    `feedback is: ${feedback}`;
+})
