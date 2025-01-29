6bd8e5e292baa2569dd6a9c1d68da19ca7b14829
iamtejasv <tejasv0804@gmail.com>
2025-01-29 13:51:59 +0530

    add challenge 7

--- script.js
+++ script.js
@@ -45,3 +45,12 @@ document.getElementById("removeLastTask").addEventListener("click", function () 
 document.getElementById("clickMeButton").addEventListener("dblclick", function () {
   alert("helloworld")
 })
+
+// challenge 7
+
+document.getElementById("teaList").addEventListener("click", function(event) {
+  if (event.target && event.target.matches('.teaItem')) {
+    alert('you selected: ' + event.target.textContent);
+  }
+  
+})
